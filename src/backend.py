f57595e49c834b3ee906839952abc764cdd5f762
Daniel Angres <gdnlclub@gmail.com>
2024-11-18 20:09:26 -0800

    Added function to load ingredients from save file

---
+++ src/backend.py
@@ -0,0 +1,65 @@
+from gameInfo import ingredientsNormalizedToProper, normalizeName
+
+
+def read_reagents(filename):
+    """
+    Reads reagents from a save file and returns them as a list of dictionaries.
+    """
+    output = {}
+    print("Collecting data...")
+    with open(filename, "rb") as infile:
+        # Skip preamble
+        infile.read(0x1F)
+        reagentsToCount = int.from_bytes(infile.read(0x1))
+        infile.read(0x4)
+        reagentCounter = 0
+        # Read in inventory data
+        for r in range(reagentsToCount):
+            wideCheck = infile.read(0x3)
+            special = wideCheck == b"\xff\xff\xff"
+            name = b""
+            buff = b""
+            while (special and buff != b"\x00\x00") or (
+                not (special) and buff != b"\x00"
+            ):
+                name += buff
+                buff = infile.read(0x2) if special else infile.read(0x1)
+            if special:
+                name = name.decode("utf-16")
+            else:
+                name = name.decode("utf-8")
+            # read unknown0
+            infile.read(0xE0)
+
+            unknown0 = infile.read(0x1)
+            infile.read(0x3)
+
+            # read unknown1
+            unknown1 = []
+            for i in range(int.from_bytes(unknown0) + 2):
+                buff = infile.read(0x1)
+                unknown1.append(int.from_bytes(buff))
+
+            infile.read(0x3)
+            magiminsEntries = unknown1[-1]
+
+            # read magimins
+            magimins = {}
+            for i in range(magiminsEntries):
+                magiminType = "ABCDE"[int.from_bytes(infile.read(0x1))]
+                magiminAmount = int.from_bytes(infile.read(0x1))
+                magimins[magiminType] = magiminAmount
+                infile.read(0x3)
+            infile.read(0x9)
+            quantity = int.from_bytes(infile.read(0x1))
+            infile.read(0x3)
+            unknown3 = int.from_bytes(infile.read(0x1))
+            reagentCounter += 1
+
+            # display ingredient, quantity
+            # print(name, unknown2, unknown0, unknown1, unknown3, sep=",")
+            output[name] = quantity
+    result = {
+        ingredientsNormalizedToProper[normalizeName(k)]: v for k, v in output.items()
+    }
+    return result
--- src/gui.py
+++ src/gui.py
@@ -8,10 +8,35 @@ def button_handler(widget):
 
 
 class Boxer(toga.App):
+    workingIngredientData = {}
+
+    async def file_select_handler(self, widget):
+        try:
+            fname = await self.dialog(toga.OpenFileDialog("Open file with Toga"))
+            if fname is not None:
+                workingIngredientData = read_reagents(fname)
+                self.ingredientsTable.data.clear()
+                tableRows = [
+                    (ingredientData[k]["name"], v)
+                    for k, v in workingIngredientData.items()
+                ][::-1]
+                for r in tableRows:
+                    self.ingredientsTable.data.insert(0, r)
+
+        except ValueError:
+            pass
 
     def startup(self):
         box = toga.Box()
 
+        easy_commands = toga.Group("Commands")
+        get_file = toga.Command(
+            self.file_select_handler,
+            text="Load Inventory",
+            tooltip="Acquire Inventory from Save File",
+            # icon=,
+            group=easy_commands,
+        )
 
         split = toga.SplitContainer()
 
@@ -28,6 +53,7 @@ class Boxer(toga.App):
         potionsPanel = toga.Box()
         box.add(potionsPanel)
 
+        self.commands.add(get_file)
         split.content = [self.ingredientsTable, potionsPanel]
         self.main_window = toga.MainWindow()
         self.main_window.content = split
